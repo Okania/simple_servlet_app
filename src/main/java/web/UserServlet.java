00e9348cbee2e3b1deb74b07463599a010ed5d86
Okania <evans.okania@strathmore.edu>
2023-01-17 11:12:30 +0300

    updated and added files

--- src/main/java/web/UserServlet.java
+++ src/main/java/web/UserServlet.java
@@ -1,5 +1,43 @@
 package web;
 
-public class UserServlet {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.User;
+import dao.UserDao;
+
+public class UserServlet extends HttpServlet{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private UserDao userDao;
+	
+	protected void doPost(HttpServletRequest req,HttpServletResponse res)
+			throws ServletException, IOException{
+		
+		String fname = req.getParameter("fname");
+		String lname = req.getParameter("lname");
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+		int age = Integer.parseInt(req.getParameter("age"));
+		
+		User user = new User(fname,lname,username,password,age);
+		
+		try {
+			userDao.registerUser(user);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
 
 }
