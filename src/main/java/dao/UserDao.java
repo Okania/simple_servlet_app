2ba1bcfc8b2bcd36bdc42150f84f86d7e5d2593e
Okania <evans.okania@strathmore.edu>
2023-01-17 22:31:20 +0300

    new updates on files

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,19 +3,19 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import model.User;
 
 public class UserDao {
 	
-	String DRIVER = "com.mysql.cj.jdbc.Driver";
-    String URL = "jdbc:mysql://localhost:3306/mytest";
-    String USER = "root";
-    String PASSWORD = "";
+	static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    static String URL = "jdbc:mysql://localhost:3306/mytest";
+    static String USER = "root";
+    static String PASSWORD = "";
     
-    public void registerUser(User user)throws SQLException, ClassNotFoundException{
-    	
+    public void registerUser(User user)throws SQLException, ClassNotFoundException{    	
     	Class.forName(DRIVER);
     	
     	try {
@@ -32,12 +32,33 @@ public class UserDao {
     		
     		statement.executeUpdate();
     		statement.close();
-    		connection.close();
-    		
+    		connection.close();    		
     		
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+    }
+    
+    public static boolean validate(String username, String pass )throws SQLException, ClassNotFoundException {
+    	
+    	
+    	boolean st = false;
+    	
+    	try {
+    		Class.forName(DRIVER);
+    		Connection connection = DriverManager.getConnection(URL,USER,PASSWORD);
+    		PreparedStatement statement = connection.prepareStatement("select * where username = ? and password = ?");
+    		statement.setString(1, username);
+    		statement.setString(2, pass);
+    		
+    		ResultSet rs = statement.executeQuery();
+    		st = rs.next();
+    		
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return st;
     }
 
 }
